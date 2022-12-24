a9884c2bd274906e04fd5bc7ec5abbcf88ae75a9
theFestest <michaelbartholic@gmail.com>
2022-12-24 12:18:58 -0500

    Completed assembly-line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -2,10 +2,18 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const PRODUCTION_RATE: f64 = 221.0;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let total = speed as f64 * PRODUCTION_RATE;
+    match speed{
+        0..=4 => total,
+        5..=8 => total * 0.9,
+        9..=10 => total * 0.77,
+        _ => 0.0,
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    production_rate_per_hour(speed) as u32 / 60
 }
