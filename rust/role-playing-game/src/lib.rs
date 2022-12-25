3fe42a7fae345a58c8feddb6d4bc893a73f42287
theFestest <michaelbartholic@gmail.com>
2022-12-25 00:31:01 -0500

    Completed role-playing-game

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -10,10 +10,38 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        if self.health == 0 {
+            if self.level >= 10 {
+                Some(Player {
+                    health: 100,
+                    mana: Some(100),
+                    level: self.level,
+                })
+            } else {
+                Some(Player {
+                    health: 100,
+                    mana: None,
+                    level: self.level,
+                })
+            }
+        } else {
+            None
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        if self.mana.is_none(){
+            if mana_cost > self.health{
+                self.health = 0;
+            } else {
+                self.health = self.health - mana_cost;
+            }
+            0
+        } else if mana_cost > self.mana.unwrap(){
+            0
+        } else {
+            self.mana = Some(self.mana.unwrap() - mana_cost);
+            2 * mana_cost
+        }
     }
 }
