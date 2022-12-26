3812152b81ba7f9a96551a91edff7439bf4aaf1a
theFestest <michaelbartholic@gmail.com>
2022-12-26 15:00:13 -0500

    Completed anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,28 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+
+    let mut characters = word.to_lowercase().chars().collect::<Vec<char>>();
+
+    let mut anagrams: HashSet<&str> = HashSet::new();
+
+    for possible_anagram in possible_anagrams.into_iter() {
+        let mut possible_anagram_characters = possible_anagram.to_lowercase().chars().collect::<Vec<char>>();
+
+        if possible_anagram.to_lowercase() == word.to_lowercase() {
+            // Skip the word itself as this is a trival anagram
+            continue;
+        }
+
+        // Prefer unstable sorting for efficiency since chars are chars
+        possible_anagram_characters.sort_unstable();
+        characters.sort_unstable();
+
+        // Anagrams to the same if they are identical after sorting
+        if possible_anagram_characters == characters {
+            anagrams.insert(possible_anagram);
+        }
+    }
+
+    anagrams
 }
